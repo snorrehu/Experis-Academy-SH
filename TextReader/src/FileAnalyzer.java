bcbff9421479b83e81e73c84935a651e60f1f28d
snorrehu <snorre.hukkelaas@gmail.com>
2018-09-12 17:49:57 +0200

    Third Commit

--- TextReader/src/FileAnalyzer.java
+++ TextReader/src/FileAnalyzer.java
@@ -3,13 +3,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileAnalyzer {
     private String fileName;
     private double fileSize;
     private int numberOfLines;
-    private String errorMessage=null;
+    private int wordcount;
 
+    //Analyze file
     public FileAnalyzer(String directory){
         try {
 
@@ -35,7 +37,26 @@ public class FileAnalyzer {
 
          //Catch the exception if the file isn't found
         }catch (IOException e1){
-            errorMessage = "File not found.";
+            System.out.println("File not found.");
+        }
+    }
+
+    //Search for words
+    public FileAnalyzer(String directory, String word){
+        Scanner scanner = null;
+        this.wordcount = 0;
+        try {
+            scanner = new Scanner(new File(directory));
+        } catch (IOException e) {
+
+        }
+        while (scanner.hasNextLine()) {
+            while (scanner.hasNext()){
+                if(word==scanner.next()){
+                    System.out.println("Found it");
+                    wordcount++;
+                }
+            }
         }
     }
 
@@ -43,5 +64,9 @@ public class FileAnalyzer {
     public String getData(){
         String output = "Filename: "+ fileName + "\n" + "Number of lines: " + numberOfLines + "\n" + "File size: " + fileSize + "kB" + "\n";
         return output;
+    }
+
+    public int getWordcount(){
+        return wordcount;
     }
 }
--- TextReader/src/TextReader.java
+++ TextReader/src/TextReader.java
@@ -19,6 +19,7 @@ Open: opens a specified file (the file will be .txt)
 */
 
 public class TextReader extends JFrame {
+    private String fileName;
 
     public static void main(String[] args) {
         TextReader frame = new TextReader();
@@ -27,53 +28,71 @@ public class TextReader extends JFrame {
     public TextReader() {
 
         this.setTitle("Text Reader Application");
-        this.setSize(400, 400);
+        this.setSize(600, 600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
 
-
-
         // Declaring Panels
-        JPanel inputPanel = new JPanel();
+        JPanel filenamePanel = new JPanel();
         JPanel outputPanel = new JPanel();
+        JPanel wordSearchPanel = new JPanel();
 
         //Setting panel layouts
-        inputPanel.setLayout(new BorderLayout());
+        filenamePanel.setLayout(new BorderLayout());
+        outputPanel.setLayout(new BorderLayout());
+        wordSearchPanel.setLayout(new BorderLayout());
 
         // Add input area
         JTextArea inputField = new JTextArea();
-
-
+        JTextArea wordSearchField = new JTextArea();
 
         //Add text output area
         JTextArea outputArea = new JTextArea(10,30);
         JScrollPane outputScrollPane = new JScrollPane(outputArea);
         outputArea.setEditable(false);
 
-        // Declare button and add listener
+
+
+        // Declare analyze button and add listener
         JButton analyzeButton = new JButton("Analyze text file");
         analyzeButton.setPreferredSize(new Dimension(10,20));
         analyzeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 outputArea.setText(null);
-                String directory = inputField.getText();
-                FileAnalyzer fileAnalyzer = new FileAnalyzer(directory);
+                fileName  = inputField.getText();
+                FileAnalyzer fileAnalyzer = new FileAnalyzer(fileName);
                 outputArea.setText(fileAnalyzer.getData());
             }
         });
-        //Adding stuff to inputPanel
-        inputPanel.add(inputField,BorderLayout.NORTH);
-        inputPanel.add(analyzeButton, BorderLayout.SOUTH);
+
+        //Declare search button and add listener
+        JButton searchButton = new JButton("Search for word");
+        searchButton.setPreferredSize(new Dimension(10,20));
+        searchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String word = wordSearchField.getText();
+                System.out.println("Searching in file: " + fileName);
+                FileAnalyzer fileAnalyzer = new FileAnalyzer(fileName,word);
+                outputArea.setText("Number of matching words: " + fileAnalyzer.getWordcount());
+            }
+        });
+
+        //Adding stuff to fileNamePanel
+        filenamePanel.add(inputField,BorderLayout.NORTH);
+        filenamePanel.add(analyzeButton,BorderLayout.SOUTH);
+
+        //Adding stuff to wordSearchPanel:
+        wordSearchPanel.add(wordSearchField,BorderLayout.NORTH);
+        wordSearchPanel.add(searchButton,BorderLayout.SOUTH);
 
         //Adding stuff to outputPanel
         outputPanel.add(outputScrollPane);
 
         //Adding panels to frame
-        this.add(inputPanel, BorderLayout.NORTH);
-        this.add(outputPanel, BorderLayout.CENTER);
-
-
+        this.add(filenamePanel,BorderLayout.NORTH);
+        this.add(wordSearchPanel,BorderLayout.CENTER);
+        this.add(outputPanel,BorderLayout.SOUTH);
 
 
         setVisible(true);
