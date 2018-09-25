3b86231d23eb665bbe5aa5c36f371bc2686dc4ba
snorrehu <snorre.hukkelaas@gmail.com>
2018-09-25 15:04:53 +0200

    test

---
+++ Elevatorsaga/elevatorSaga.js
@@ -0,0 +1,14 @@
+{
+    init: function(elevators, floors) {
+        var elevator1 = elevators[0]; 
+        
+        elevators.forEach(function(elevator)){
+                                  
+        }
+        
+        
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
+}
