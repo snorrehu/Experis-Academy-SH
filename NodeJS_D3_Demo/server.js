37cf3ef8db15fd0534d24d9554f2bd268ac073ae
snorrehu <snorre.hukkelaas@gmail.com>
2018-09-26 21:13:47 +0200

    Added user registration webpage

---
+++ NodeJS_D3_Demo/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.use( express.static(__dirname + '/views'));
+
+
+app
+  .get( '/', function( req, res ) {
+    res.sendFile( path.join( __dirname, 'views', 'index.html' ));
+  });
+
+app.listen(8080);
--- README.md
+++
@@ -1,1 +0,0 @@
-# Experis-Academy-SH
